f12be1de043173c29db3a8c5bfe79416f2cec896
wagnrrt <wagnrb0@gmail.com>
2025-08-08 20:14:55 -0300

    refactor: update ts-ls configuration

--- nvim/lsp/ts_ls.lua
+++ nvim/lsp/ts_ls.lua
@@ -1,4 +1,5 @@
 return {
+	init_options = { hostInfo = "neovim" },
 	cmd = { "typescript-language-server", "--stdio" },
 	filetypes = {
 		"javascript",
@@ -8,15 +9,61 @@ return {
 		"typescriptreact",
 		"typescript.tsx",
 	},
-	root_markers = {
-		"tsconfig.json",
-		"jsconfig.json",
-		"package.json",
-		".git",
+	root_markers = { "tsconfig.json", "jsconfig.json", "package.json", ".git" },
+	handlers = {
+		-- handle rename request for certain code actions like extracting functions / types
+		["_typescript.rename"] = function(_, result, ctx)
+			local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+			vim.lsp.util.show_document({
+				uri = result.textDocument.uri,
+				range = {
+					start = result.position,
+					["end"] = result.position,
+				},
+			}, client.offset_encoding)
+			vim.lsp.buf.rename()
+			return vim.NIL
+		end,
 	},
-	init_options = {
-		hostInfo = "neovim",
+	commands = {
+		["editor.action.showReferences"] = function(command, ctx)
+			local client = assert(vim.lsp.get_client_by_id(ctx.client_id))
+			local file_uri, position, references = unpack(command.arguments)
+
+			local quickfix_items = vim.lsp.util.locations_to_items(references, client.offset_encoding)
+			vim.fn.setqflist({}, " ", {
+				title = command.title,
+				items = quickfix_items,
+				context = {
+					command = command,
+					bufnr = ctx.bufnr,
+				},
+			})
+
+			vim.lsp.util.show_document({
+				uri = file_uri,
+				range = {
+					start = position,
+					["end"] = position,
+				},
+			}, client.offset_encoding)
+
+			vim.cmd("botright copen")
+		end,
 	},
-	single_file_support = true,
-	log_level = vim.lsp.protocol.MessageType.Warning,
+	on_attach = function(client, bufnr)
+		-- ts_ls provides `source.*` code actions that apply to the whole file. These only appear in
+		-- `vim.lsp.buf.code_action()` if specified in `context.only`.
+		vim.api.nvim_buf_create_user_command(bufnr, "LspTypescriptSourceAction", function()
+			local source_actions = vim.tbl_filter(function(action)
+				return vim.startswith(action, "source.")
+			end, client.server_capabilities.codeActionProvider.codeActionKinds)
+
+			vim.lsp.buf.code_action({
+				context = {
+					only = source_actions,
+				},
+			})
+		end, {})
+	end,
 }
